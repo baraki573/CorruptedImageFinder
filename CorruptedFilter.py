881853ec00c213a2c35487b1cd450ab5aba45803
masterofobzene <94469116+masterofobzene@users.noreply.github.com>
2022-05-11 15:49:41 -0300

    Add files via upload

---
+++ CorruptedFilter.py
@@ -0,0 +1,35 @@
+from os import listdir
+import os
+import cv2
+import shutil
+from collections import Counter
+
+source_input = input("Please drag & drop the directory containing CORRUPTED images: ")
+output = "E:\XXXXXXX\XXXXXXX\XXXXXXX" + '\\'
+counter = 0
+
+def nooutput():
+     if os.path.exists(output):
+        process()
+     else:
+        os.mkdir(output)
+        process()
+        
+def process():
+    for filename in listdir(source_input):
+        if filename.endswith(("jpg", "jpeg", "bmp", "png")):
+            try: 
+                img = (source_input+'\\'+filename)
+                read = cv2.imread(source_input+'\\'+filename)
+                cv2.imshow("IMAGE", read)
+            except cv2.error as e:
+                if e.err == "!_src.empty()":
+                    global counter
+                    counter+=1
+                    print('ERROR ON FILE: ' + img + '  taking it out')
+                    shutil.move(img, output)
+
+nooutput()
+
+print('CORRUPTED IMAGES = ', counter)
+input("Press a key to close...")
