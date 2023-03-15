aac71415d3d61c34550ab791b71cfab0d476dccb
masterofobzene <94469116+masterofobzene@users.noreply.github.com>
2023-03-15 01:14:59 +0000

    Update CorruptedFilter.py

--- CorruptedFilter.py
+++ CorruptedFilter.py
@@ -1,11 +1,20 @@
 from os import listdir
 import os
-import cv2
+#import cv2
+from PIL import Image
 import shutil
 from collections import Counter
+import glob
 
 source_input = input("Please drag & drop the directory containing CORRUPTED images: ")
-output = "E:\XXXXXXX\XXXXXXX\XXXXXXX" + '\\'
+source_dir = (source_input)
+source_dir = source_dir.strip('"')
+source_dir2 = source_dir.replace('[', 'o[o')
+source_dir2 = source_dir2.replace(']', 'o]o')
+source_dir2 = source_dir2.replace('o[o', '[[]')
+source_dir2 = source_dir2.replace('o]o', '[]]')
+source_dirclean = source_dir2.replace('#', '[#]')
+output = "E:\XXXXX\XXXXXXXX\XXXXXXXX" + '\\'
 counter = 0
 
 def nooutput():
@@ -16,16 +25,20 @@ def nooutput():
         process()
         
 def process():
-    for filename in listdir(source_input):
-        if filename.endswith(("jpg", "jpeg", "bmp", "png")):
-            try: 
+    for filename in glob.iglob(source_dirclean + '/**/*.*', recursive=True):
+        if filename.endswith(("jpg", "JPG", "jpeg", "JPEG", "bmp", "BMP", "png", "PNG")):
+            #breakpoint()
+            try:
                 img = (filename)
-                read = cv2.imread(img)
-                cv2.imshow("IMAGE", read)
-                cv2.waitKey(50)
-                cv2.destroyAllWindows()
-            except cv2.error as e:
-                if e.err == "!_src.empty()":
+                imge = Image.open(filename)
+                imge.verify()
+                #read = cv2.imread(img)
+                #cv2.imshow("IMAGE", read)
+                #cv2.waitKey(50)
+                #cv2.destroyAllWindows()
+            except (IOError, SyntaxError) as e:
+            #except cv2.error as e:
+                #if e.err == "!_src.empty()":
                     global counter
                     counter+=1
                     print('ERROR ON FILE: ' + img + '  taking it out')
